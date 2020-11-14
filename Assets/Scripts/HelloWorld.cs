2147c6c52649eaaebba02e9a097cd42b35c37b9b
Janina-sh <janina.pls@googlemail.com>
2020-11-14 12:47:01 +0100

    game logic

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    [SerializeField] private int m_threshold = 10;    // put it into inspector
+    [SerializeField] private string m_message = "Hello World";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LogHelloWorld(m_message);  //print Hello World when in Game Mode
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LogHelloWorld(string message)  // void means here nothing will return, it only executes
+    {
+        if (m_threshold > 10)
+        Debug.Log(message);   // Ausführen von Log.message
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;         // add this for Input being able 
+
+public class PlayerController : MonoBehaviour
+{
+    
+    [SerializeField] private float m_speed = 1f;
+    private Rigidbody m_playerRigidbody;
+    
+    private float m_movementX;
+    private float m_movementY;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_playerRigidbody = GetComponent<Rigidbody>();       // Unity checks if there is a Rigidbody Component added to our Gameobject this script is attached to (here Player). If so, it will add it to the Variable m_playerRigidbody  
+    }
+
+
+    private void OnMove(InputValue inputValue)                // When a user input happens, we do the Vector2 movement by storing the input values in the variables
+    {
+        Vector2 movementVector = inputValue.Get<Vector2>();
+
+        m_movementX = movementVector.x;
+        m_movementY = movementVector.y;
+    }
+
+
+    private void FixedUpdate()                                     
+    {
+        Vector3 movement = new Vector3(m_movementX, 0f, m_movementY);     // Set like to Player movement
+        
+        m_playerRigidbody.AddForce(movement * m_speed);                // apply this Movement Vector to our Rigidbody
+    }
+
+    private void OnTriggerEnter(Collider other) // our collectable (cube) gets deactivated when this method is called 
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
