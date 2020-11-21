6d74481a919830749bc3874c6248ac6929bd692c
Janina-sh <janina.pls@googlemail.com>
2020-11-21 19:03:39 +0100

    Minimal- und Maximalwert für Playersphere

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,30 +19,39 @@ public class PlayerController : MonoBehaviour
     private int m_collectablesCounter;
 
     private Stopwatch m_stopwatch;
+    
+    private float m_localScaleMax;
+    private float m_localScaleMin;
 
-///// [SerializeField] private InputAction m_inputaction;
-////private float m_localScaleMax;
-
+    
+    
     void Start()
     {
-        m_playerRigidbody = GetComponent<Rigidbody>();       // Unity checks if there is a Rigidbody Component added to our Gameobject this script is attached to (here Player). If so, it will add it to the Variable m_playerRigidbody  
-
+        // Unity checks if there is a Rigidbody Component added to our Gameobject this script is attached to (here Player).
+        // If so, it will add it to the Variable m_playerRigidbody  
+        m_playerRigidbody = GetComponent<Rigidbody>();
+        
+       
         m_collectablesTotalCount = m_collectablesCounter = GameObject.FindGameObjectsWithTag("Collectable").Length;
         
         m_stopwatch = Stopwatch.StartNew();
-      
-
-////////m_localScaleMax = 1.2f;
+        
+        m_localScaleMax = 1.3f; 
+        m_localScaleMin = 0.7f;
     }
 
+    
 
-    private void OnMove(InputValue inputValue)                // When a user input happens, we do the Vector2 movement by storing the input values in the variables
+    // When a user input happens, we do the Vector2 movement by storing the input values in the variables
+    private void OnMove(InputValue inputValue)               
     {
         Vector2 movementVector = inputValue.Get<Vector2>();
 
         m_movementX = movementVector.x;
         m_movementY = movementVector.y;
     }
+    
+    
     
     private void FixedUpdate()                                     
     {
@@ -52,7 +61,10 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other) // our collectable (cube) gets deactivated when this method is called 
+    
+    
+    // our collectable (cube) gets deactivated when this method is called
+    private void OnTriggerEnter(Collider other)  
     {
         if (other.gameObject.CompareTag("Collectable"))
         {
@@ -60,41 +72,39 @@ public class PlayerController : MonoBehaviour
 
             m_collectablesCounter--;
 
-////////////if (transform.localScale.x >= m_localScaleMax)
-////////////{
-/////////////////gameObject.transform.localScale = m_localScaleMax;
-////////////}
             
-            //increase size of player when colliding with collectable
-            gameObject.transform.localScale += new Vector3(.1f, .1f, .1f);
+            //increase size of playersphere until max-value
+            if (gameObject.transform.localScale.x < m_localScaleMax)
+            {
+                gameObject.transform.localScale += new Vector3(.1f, .1f, .1f);
+            }
 
 
             if (m_collectablesCounter == 0)
-        {
+            {
             Debug.Log("YOU WIN! CONGRATULATIONS!");
             Debug.Log($"It took you {m_stopwatch.Elapsed} to find all {m_collectablesTotalCount} collectables.");
                 
-#if UNITY_EDITOR
+            #if UNITY_EDITOR
             UnityEditor.EditorApplication.ExitPlaymode();           
-#endif  
-        }
-        else
-        {
-            Debug.Log(
-                $"You've already found {m_collectablesTotalCount - m_collectablesCounter} of {m_collectablesTotalCount} collectables!");
-        }
+            #endif  
+            }
+            
+            else
+            {
+                Debug.Log($"You've already found {m_collectablesTotalCount - m_collectablesCounter} of {m_collectablesTotalCount} collectables!");
+            }
+            
         }
         
         
-
         else if (other.gameObject.CompareTag("Enemy"))
         {
             Debug.Log("GAME OVER! MUAHAHA!");
             
-#if UNITY_EDITOR
-            UnityEditor.EditorApplication.ExitPlaymode();           
-#endif        
-
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.ExitPlaymode();           
+            #endif
         }
 
         //our toxic collectable gets deactivated when this method is called 
@@ -102,13 +112,13 @@ public class PlayerController : MonoBehaviour
         {
             other.gameObject.SetActive(false);
             Debug.Log("NOO, UGH, Doesn't taste good!");
-
-        //decrease size of player when colliding with collectable
-        gameObject.transform.localScale += new Vector3(-.1f, -.1f, -.1f);
+            
+            //decrease size of playersphere until min-value
+            if (gameObject.transform.localScale.x > m_localScaleMin)
+                { 
+                    gameObject.transform.localScale += new Vector3(-.1f, -.1f, -.1f);
+                }
         }
-
-       //////////////if (gameObject.transform.localScale = m_localScaleMax)
         
-
     }
 }
