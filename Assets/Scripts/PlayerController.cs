7f8e7abf31d63776056b13e92a87cb8bfcc5f8fb
Janina-sh <janina.pls@googlemail.com>
2021-01-16 21:00:08 +0100

    Restart nach dem Runterfallen anstatt End Game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -76,9 +76,7 @@ public class PlayerController : MonoBehaviour
         // Beendet das spiel, falls die y-position des players  -4 ist:
         if (transform.position.y < -4)
         {
-#if UNITY_EDITOR
-            UnityEditor.EditorApplication.ExitPlaymode();
-#endif
+            Application.LoadLevel(Application.loadedLevel);
         }
 
         Vector3 movement = new Vector3(m_movementX, 0f, m_movementY);     // Set like to Player movement
