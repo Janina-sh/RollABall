0d886fe877516125b08f10f5902db9886dd094d9
MarieKunzmann <73614805+MarieKunzmann@users.noreply.github.com>
2021-01-23 16:26:12 +0100

    try 10102103003

--- Assets/Scripts/OSCDummy.cs
+++ Assets/Scripts/OSCDummy.cs
@@ -3,6 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using extOSC;
 
+public class OSCDummy : MonoBehaviour
+{
+    public string Address = "/example/1";
+    public OSCReceiver Receiver;
+    public PlayerController playerController;
+    public GameObject ground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Receiver.Bind(Address, ReceivedMessage);
+    }
+
+    private void ReceivedMessage(OSCMessage message)
+    {
+        //Vector2 touch;
+        Quaternion rotation;
+
+        //Vector3 tmp;
+
+        if (message.ToQuaternion(out rotation))
+        {
+            //SEB: This should only pass a message to your GroundController . 
+            // The GroundController needs to implement the rotation/interpolation behavior.
+            ground.GetComponent<GroundController>().SetTargetRotation(rotation);
+            Debug.Log(rotation);
+        }
+
+
+
+    }
+}
+
 //public class OSCDummy : MonoBehaviour
 //{
 //    public string Address = "/example/1";
@@ -31,46 +64,46 @@ using extOSC;
 //}
 
 
-public class OSCDummy : MonoBehaviour
-{
-    public string Address = "/example/1";
-    public OSCReceiver Receiver;
-    public PlayerController playerController;
-    public GameObject ground;
+//public class OSCDummy : MonoBehaviour
+//{
+//    public string Address = "/example/1";
+//    public OSCReceiver Receiver;
+//    public PlayerController playerController;
+//    public GameObject ground;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Receiver.Bind(Address, ReceivedMessage);
-    }
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        Receiver.Bind(Address, ReceivedMessage);
+//    }
 
-    private void ReceivedMessage(OSCMessage message)
-    {
-        Vector2 touch;
-        Vector4 rotation;
+//    private void ReceivedMessage(OSCMessage message)
+//    {
+//        Vector2 touch;
+//        Vector4 rotation;
 
-        Vector3 tmp;
+//        Vector3 tmp;
 
 
-        //Debug.Log(message.ToVector2(out touch));
+//        //Debug.Log(message.ToVector2(out touch));
 
-        if (message.ToVector4(out rotation)==true)
-        {
-            //tmp = rotation.eulerAngles; //changing the quaternion to euler rotation (rotation = quaternion)
+//        if (message.ToVector4(out rotation)==true)
+//        {
+//            //tmp = rotation.eulerAngles; //changing the quaternion to euler rotation (rotation = quaternion)
 
-            //ground.transform.rotation = Quaternion.Euler(-tmp.x,tmp.z,-tmp.y); //creating a quaternion out of this euler rotation
-            //ground.GetComponent<Rigidbody>().AddTorque(-tmp.x, tmp.z, -tmp.y);
-            playerController.OnMoveVector4(rotation);
-            //Debug.Log(rotation);
-        }
+//            //ground.transform.rotation = Quaternion.Euler(-tmp.x,tmp.z,-tmp.y); //creating a quaternion out of this euler rotation
+//            //ground.GetComponent<Rigidbody>().AddTorque(-tmp.x, tmp.z, -tmp.y);
+//            playerController.OnMoveVector4(rotation);
+//            //Debug.Log(rotation);
+//        }
 
-         if (message.ToVector2(out touch) == true)
-                {
-                    playerController.OnMoveVector2(touch);
-                     //Debug.Log(touch);
-                 }
-    }
-}
+//         if (message.ToVector2(out touch) == true)
+//                {
+//                    playerController.OnMoveVector2(touch);
+//                     //Debug.Log(touch);
+//                 }
+//    }
+//}
 //public class OSCDummy : MonoBehaviour
 //{
 //    public string Address = "/example/1";
@@ -106,35 +139,35 @@ public class OSCDummy : MonoBehaviour
 //            tmp = rotation.eulerAngles; //changing the quaternion to euler rotation (rotation = quaternion)
 
 //            ground.transform.rotation = Quaternion.RotateTowards(-tmp.x, tmp.z, -tmp.y); //creating a quaternion out of this euler rotation
-           
+
 
 //            Debug.Log(rotation);
 //        }
 
-        //{
-        //    // The object whose rotation we want to match.
-        //    Transform target;
+//{
+//    // The object whose rotation we want to match.
+//    Transform target;
 
-        //    // Angular speed in degrees per sec.
-        //    float speed;
+//    // Angular speed in degrees per sec.
+//    float speed;
 
-        //    void Update()
-        //    {
-        //        // The step size is equal to speed times frame time.
-        //        var step = speed * Time.deltaTime;
+//    void Update()
+//    {
+//        // The step size is equal to speed times frame time.
+//        var step = speed * Time.deltaTime;
 
-        //        // Rotate our transform a step closer to the target's.
-        //        transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, step);
-        //    }
+//        // Rotate our transform a step closer to the target's.
+//        transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, step);
+//    }
 
 
-        //    //tmp = rotation.eulerAngles; //changing the quaternion to euler rotation (rotation = quaternion)
+//    //tmp = rotation.eulerAngles; //changing the quaternion to euler rotation (rotation = quaternion)
 
-        //    //ground.transform.rotation = Quaternion.RotateTowards(-tmp.x,tmp.z,-tmp.y); //creating a quaternion out of this euler rotation
-        //    //ground.GetComponent<Rigidbody>().AddRelativeTorque(-tmp.x, tmp.z, -tmp.y);
+//    //ground.transform.rotation = Quaternion.RotateTowards(-tmp.x,tmp.z,-tmp.y); //creating a quaternion out of this euler rotation
+//    //ground.GetComponent<Rigidbody>().AddRelativeTorque(-tmp.x, tmp.z, -tmp.y);
 
-        //    //Debug.Log(rotation);
-        //}
+//    //Debug.Log(rotation);
+//}
 
 
 //        if (message.ToVector2(out touch) == true)
